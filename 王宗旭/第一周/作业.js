624dba332c552f190a2a95fe8a5d008d24a02a1a
王宗旭 <308929264@qq.com>
2016-07-12 16:01:34 +0800

    Update 作业.js

--- 王宗旭/第一周/作业.js
+++ 王宗旭/第一周/作业.js
@@ -1,6 +1,6 @@
 //异步创建文件夹
-function mkdir(path, index) {
-    index = index || 1;
+function mkdir(path){
+    index = arguments[1] || 1;
     var ary = path.split('/');
     if (index > ary.length) {
         return
